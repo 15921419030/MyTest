8220303378f9a8b3ab21111e5085e4ad35448e6a
15921419030 <15921419030@163.com>
2012-07-20 11:42:32 +0800

    wangbf edit project
    Signed-off-by: 15921419030 <15921419030@163.com>

---
+++ NhibernateWeb/AspnetpagerTest.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="AspnetpagerTest.aspx.vb" Inherits="NhibernateWeb.AspnetpagerTest" %>
+
+<%@ Register assembly="AspNetPager" namespace="Wuqi.Webdiyer" tagprefix="webdiyer" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+
+        <webdiyer:AspNetPager ID="AspNetPager1" runat="server" horizontalalign="Center" width="100%" ShowPageIndexBox="Always"
+        EnableUrlRewriting="true" UrlRewritePattern="listpage_{0}.html" OnPageChanged="AspNetPager1_PageChanged" NumericButtonTextFormatString="-{0}-">
+        </webdiyer:AspNetPager>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ NhibernateWeb/AspnetpagerTest.aspx.vb
@@ -0,0 +1,34 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class AspnetpagerTest
+    Inherits System.Web.UI.Page
+
+    Dim conn As String
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        conn = "Data Source=LBWIN7;Initial Catalog=AspNetPageDB;User ID=sa;Password=sa"
+
+        If Not IsPostBack Then
+            'cache the number of total records to improve performance
+            Dim obj As Object = Cache(Convert.ToString([GetType]()) & "totalOrders")
+            If obj Is Nothing Then
+                Dim totalOrders As Integer = CInt(SqlHelper.ExecuteScalar(conn, "P_GetOrderNumber", Nothing))
+                Cache(Convert.ToString([GetType]()) & "totalOrders") = totalOrders
+                AspNetPager1.RecordCount = totalOrders
+            Else
+                AspNetPager1.RecordCount = CInt(obj)
+            End If
+        End If
+
+    End Sub
+
+    Protected Sub AspNetPager1_PageChanged(ByVal sender As Object, ByVal e As EventArgs) Handles AspNetPager1.PageChanged
+        GridView1.DataSource = SqlHelper.ExecuteReader(conn, "P_GetPagedOrders2000", New SqlParameter("@startIndex", AspNetPager1.StartRecordIndex), New SqlParameter("@endIndex", AspNetPager1.EndRecordIndex))
+        GridView1.DataBind()
+
+    End Sub
+
+End Class
